6aa985d8e494cf8183b7966022e473e37d9d5bb7
Carlos Penagos <carlos.penagos@uptc.edu.co>
2026-03-01 20:41:58 -0500

    (feat) (Deploy): add GitHub Actions workflow for deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export', // Genera archivos estáticos
+  images: {
+    unoptimized: true, // Necesario para GitHub Pages
+  },
 };
 
 export default nextConfig;
